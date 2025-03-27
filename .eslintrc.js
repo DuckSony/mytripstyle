20d58dcba37b8cfb0be37020633c6aa039699379
DuckSony <ducksony7@gmail.com>
2025-03-27 09:00:32 +0900

    Fix ESLint configuration by removing Cypress references

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,41 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      'cypress/globals': true,
-      'jest': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'react-app',
+    'react-app/jest'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:cypress/recommended',
-      'react-app',
-      'react-app/jest'
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 2022,
-      sourceType: 'module'
-    },
-    plugins: [
-      'react',
-      'cypress'
-    ],
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'warn',
-      'react/react-in-jsx-scope': 'off', // React 17+ 에서는 import React 가 필요 없음
-      'react/prop-types': 'off' // 필요에 따라 활성화
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
-    ignorePatterns: [
-      'node_modules/',
-      'build/',
-      'dist/',
-      'public/'
-    ]
-  };
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    'react/react-in-jsx-scope': 'off', // React 17+ 에서는 import React 가 필요 없음
+    'react/prop-types': 'off' // 필요에 따라 활성화
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'build/',
+    'dist/',
+    'public/'
+  ]
+};
